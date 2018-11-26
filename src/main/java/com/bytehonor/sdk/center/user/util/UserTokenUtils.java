dc4d718eb021d779d9920bfa158ad2249b986ac8
lijianqiang <newphper@gmail.com>
2018-11-26 21:21:39 +0800

    bug

--- src/main/java/com/bytehonor/sdk/center/user/util/UserTokenUtils.java
+++ src/main/java/com/bytehonor/sdk/center/user/util/UserTokenUtils.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import com.bytehonor.sdk.center.user.constant.UserProfileTypeEnum;
 import com.bytehonor.sdk.center.user.model.UserToken;
@@ -27,6 +28,10 @@ public class UserTokenUtils {
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("Authentication:{}", auth);
 		}
+		if (StringUtils.isEmpty(auth)) {
+			throw new RuntimeException("Authentication is null");
+		}
+		
 		String[] arr = auth.split(CON);
 		if (arr == null || arr.length < 3) {
 			throw new RuntimeException("Authentication is invalid");
@@ -34,7 +39,7 @@ public class UserTokenUtils {
 		Integer typeVal = Integer.valueOf(arr[0]);
 		UserProfileTypeEnum type = UserProfileTypeEnum.typeOf(typeVal);
 		if (UserProfileTypeEnum.UNKNOWN.getType() == type.getType()) {
-			throw new RuntimeException("Authentication content is invalid");
+			throw new RuntimeException("Authentication type is invalid");
 		}
 
 		String guid = arr[1];
