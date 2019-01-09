272b8659df572ddfe648dce96f190a4af0a34667
lijianqiang <newphper@gmail.com>
2019-01-09 12:01:58 +0800

    opt

--- src/main/java/com/bytehonor/sdk/center/user/util/UserPassportUtils.java
+++ src/main/java/com/bytehonor/sdk/center/user/util/UserPassportUtils.java
@@ -1,5 +1,9 @@
 package com.bytehonor.sdk.center.user.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.util.StringUtils;
@@ -9,7 +13,7 @@ import com.bytehonor.sdk.center.user.model.UserPassport;
 
 public class UserPassportUtils {
 
-	private static final String CON = "&";
+	private static final char CON = '&';
 
 	public static UserPassport build(Integer profileType, String guid, String fromIp, String fromTerminal) {
 		UserPassport up = new UserPassport();
@@ -28,18 +32,63 @@ public class UserPassportUtils {
 
 	public static UserPassport get(HttpServletRequest request) {
 		String val = request.getHeader(UserHeaderKey.X_USER_PASSPORT);
+		return get(val);
+	}
+
+	public static UserPassport get(String val) {
 		UserPassport up = new UserPassport();
 		if (StringUtils.isEmpty(val)) {
 			return up;
 		}
-		String[] arr = val.split(CON);
-		if (arr == null || arr.length != 4) {
+		List<String> arr = split(val);
+		if (arr == null || arr.size() != 4) {
 			return up;
 		}
-		up.setProfileType(Integer.valueOf(arr[0]));
-		up.setGuid(arr[1]);
-		up.setFromTerminal(arr[2]);
-		up.setFromIp(arr[3]);
+		up.setProfileType(Integer.valueOf(arr.get(0)));
+		up.setGuid(arr.get(1));
+		up.setFromTerminal(arr.get(2));
+		up.setFromIp(arr.get(3));
 		return up;
+	}
+
+	/**
+	 * 比 src.spilt() 会更快一些
+	 * 
+	 * @param src
+	 * @return
+	 */
+	public static List<String> split(String src) {
+		Objects.requireNonNull(src, "src");
+		int length = src.length();
+		List<String> res = new ArrayList<String>();
+		int begin = 0;
+		int count = 0;
+		boolean findOne = false;
+		boolean beginNewOne = true;
+		char[] charArray = src.toCharArray();
+		for (int i = 0; i < length; i++) {
+			if (CON != charArray[i]) {
+				if (beginNewOne) {
+					begin = i;
+					beginNewOne = false;
+				}
+				count++;
+				findOne = false;
+			} else {
+				findOne = true;
+			}
+
+			if (findOne && count > 0) {
+				res.add(new String(charArray, begin, count));
+				count = 0;
+				beginNewOne = true;
+			}
+		}
+
+		if (count > 0) {
+			res.add(new String(charArray, begin, count));
+		}
+
+		return res;
 	}
 }
---
+++ src/test/java/com/bytehonor/sdk/center/user/util/UserPassportUtilsTest.java
@@ -0,0 +1,48 @@
+package com.bytehonor.sdk.center.user.util;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bytehonor.sdk.center.user.model.UserPassport;
+
+public class UserPassportUtilsTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(UserPassportUtilsTest.class);
+
+	@Test
+	public void testToString() {
+		String src = UserPassportUtils.toString(1, "guid", "fromTerminal", "fromIp");
+		LOG.info("src:{}", src);
+		assertTrue("*testToString*", src != null);
+	}
+
+	@Test
+	public void testGetString() {
+		UserPassport up = UserPassportUtils.get("1&guid&fromTerminal&fromIp");
+		LOG.info("getGuid:{}", up.getGuid());
+		assertTrue("*testToString*", up.getGuid() != null);
+	}
+
+	@Test
+	public void testTime() {
+		String src = "1&guid&fromTerminal&fromIp";
+		int total = 1000;
+		long start1 = System.nanoTime();
+		for (int i = 0; i < total; i++) {
+			UserPassportUtils.split(src);
+		}
+		long cost1 = System.nanoTime() - start1;
+
+		long start2 = System.nanoTime();
+		for (int i = 0; i < total; i++) {
+			src.split("&");
+		}
+		long cost2 = System.nanoTime() - start2;
+
+		LOG.info("cost1:{}, cost2:{}, diff:{}", cost1, cost2, (cost1 - cost2));
+		assertTrue("*testTime*", true);
+	}
+}
