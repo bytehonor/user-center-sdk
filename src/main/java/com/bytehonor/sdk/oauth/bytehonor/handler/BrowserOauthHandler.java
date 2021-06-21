da798d23e663f5ac6d735179b4ccc558f41d7190
lijianqiang <newphper@126.com>
2021-06-21 11:02:11 +0800

    getDebug

--- src/main/java/com/bytehonor/sdk/oauth/bytehonor/handler/BrowserOauthHandler.java
+++ src/main/java/com/bytehonor/sdk/oauth/bytehonor/handler/BrowserOauthHandler.java
@@ -1,9 +1,13 @@
 package com.bytehonor.sdk.oauth.bytehonor.handler;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+import com.bytehonor.sdk.define.bytehonor.error.TokenExpiredExcption;
 import com.bytehonor.sdk.oauth.bytehonor.constant.BytehonorTerminalEnum;
 import com.bytehonor.sdk.oauth.bytehonor.model.OauthResult;
+import com.bytehonor.sdk.oauth.bytehonor.util.AccessTokenUtils;
+import com.bytehonor.sdk.oauth.bytehonor.model.AccessTokenBody;
 import com.bytehonor.sdk.oauth.bytehonor.model.OauthRequest;
 
 public class BrowserOauthHandler implements OauthHandler {
@@ -17,7 +21,12 @@ public class BrowserOauthHandler implements OauthHandler {
     public OauthResult handle(OauthRequest request) {
         Objects.requireNonNull(request.getDebug(), "debug");
 
-        return OauthResult.permit(null, null);
+        AccessTokenBody body = AccessTokenUtils.parse(request.getDebug()); // 放在debug
+        LocalDateTime expired = LocalDateTime.now().minusHours(2L);
+        if (expired.isAfter(body.getTime())) {
+            throw new TokenExpiredExcption();
+        }
+        return OauthResult.permit(body.getUuid(), null);
     }
 
 }
